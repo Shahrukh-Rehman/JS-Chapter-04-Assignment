92e673fbe8e61df3bdd2c09b63b6953b620f1a89
Shahrukh-Rehman <shahrukhrehman321@gmail.com>
2023-09-10 01:09:51 +0500

    Question 1 to 3

---
+++ app.js
@@ -0,0 +1,19 @@
+//1
+// var value1,value2,value3;
+//2
+//legal
+// var looks
+// var get_a_look
+// var $looks
+// var _looks
+// var look123
+//illegal
+// var get-a-look
+// var 123looks
+// var get a look
+// var looks 123
+// var const
+//3
+// document.write("Rules for naming JS variables ".bold()+"<br>")
+// document.write("<br>"+"Variables name can only contain, numbers, $ and _. For example : $my_1stVariable"+"<br>"+"Variable must begin with a letter, $ or _. For example : $name,_name or name"+"<br>"+"Variable names are case sensitive"+"<br>"+"Variable names should not be JS keywords")
+
